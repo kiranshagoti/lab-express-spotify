0173d967ef30b0d82d1b9ed51e64e67c4abac785
Kiran Shagoti <kiranmshagoti@Kirans-Air.zyxel.box>
2019-09-08 21:23:41 +0200

    first commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const hbs = require('hbs');
+const clientId='d3181bdee67e4762b8593e70929fe284',
+clientSecret ='f7848f1323204093979bafc6d9c9fc28';
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
 
-
+const spotifyApi = new SpotifyWebApi({
+    clientId : clientId,
+    clientSecret : clientSecret
+  });
+hbs.registerPartials(__dirname + '/views/partials');
 
 const app = express();
 
@@ -13,13 +20,34 @@ app.use(express.static(__dirname + '/public'));
 
 
 // setting the spotify-api goes here:
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
-
-
+  
 
 
 
 // the routes go here:
+app.get("/", (req, res) => {
+    res.render("index.hbs");
+  });
+
+app.get('/artist', (req, res, next) => {
+  let artist = req.query.search
+  spotifyApi.searchArtists(artist)
+    .then(data => {
+      let artist = data.body.artists.items;
+      res.render('artists', { artist })
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+})
 
 
 
